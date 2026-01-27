5d117b9ed45bae45edd926e73fb73a6c86e880da
Jean Victor <jeanvictor.fa@gmail.com>
2026-01-27 18:50:19 -0300

    :bug: Cuidados com Classes muito grandes

---
+++ src/advanceds/07_bad_code.ts
@@ -0,0 +1,6 @@
+//Código grande com muitos problemas de legibilidade e manutenção
+//Muitas responsabilidades em uma única função
+//Nomes de variáveis e funções pouco descritivos
+//Falta de modularização
+//Comentários excessivos e desnecessários
+//Classes muito grandes e complexas
